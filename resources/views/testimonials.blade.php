ac1352f5119757a6ae0beae8684bd2927ca309cc
Andrew Atkinson <andrew.atkinson@visualsoft.co.uk>
2024-03-26 23:35:14 +0000

    added references

---
+++ resources/views/testimonials.blade.php
@@ -0,0 +1,81 @@
+<!-- Testimonials -->
+<div class="row">
+                            <div class="col-xs-12 col-sm-12">
+                                <div class="block-title">
+                                    <h3>Testimonials</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12">
+                                <div class="testimonials owl-carousel">
+                                    <!-- Testimonial 1 -->
+                                    <div class="testimonial">
+                                        <div class="img">
+                                            <img src="img/testimonials/testimonial-1.jpg" alt="Andrew Atkinson">
+                                        </div>
+                                        <div class="text">
+                                            <p>Meet Andy, the Lead PHP Developer extraordinaire, whose coding finesse
+                                                 and creative flair sculpt digital wonders. With PHP as his paintbrush, 
+                                                 he crafts seamless web experiences that dazzle and delight. A visionary leader, 
+                                                 Andy inspires teams to greatness, turning visions into reality with precision and passion</p>
+                                        </div>
+
+                                        <div class="author-info">
+                                            <h4 class="author">ChatGPT</h4>
+                                            <h5 class="company">AI Testimonials</h5>
+                                            <div class="icon">
+                                                <i class="fas fa-quote-right"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- End of Testimonial 1 -->
+
+                                    <!-- Testimonial 2 -->
+                                    <div class="testimonial">
+                                        <div class="img">
+                                            <img src="img/testimonials/testimonial-2.jpg" alt="Andrew Atkinson">
+                                        </div>
+                                        <div class="text">
+                                            <p>Step into the realm of digital innovation with Andy, the trailblazing Lead PHP Developer
+                                                 behind cutting-edge web solutions. Armed with code as his weapon, he orchestrates digital
+                                                  symphonies that mesmerize users and exceed expectations. With Andy at the helm, projects thrive, 
+                                                  fueled by his unwavering dedication and unparalleled expertise.</p>
+                                        </div>
+
+                                        <div class="author-info">
+                                            <h4 class="author">ChatGPT</h4>
+                                            <h5 class="company">AI Testimonials</h5>
+                                            <div class="icon">
+                                                <i class="fas fa-quote-right"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- End of Testimonial 2 -->
+
+                                    <!-- Testimonial 3 -->
+                                    <div class="testimonial">
+                                        <div class="img">
+                                            <img src="img/testimonials/testimonial-3.jpg" alt="Andrew Atkinson">
+                                        </div>
+                                        <div class="text">
+                                            <p>Meet Andy, the maestro of PHP development, whose boundless creativity and technical acumen
+                                                 redefine digital landscapes. As a Lead PHP Developer, he architects flawless web experiences
+                                                  that leave a lasting impression. A collaborative leader and relentless problem solver, Andy
+                                                   leads teams to victory, blending innovation and expertise to deliver unparalleled results</p>
+                                        </div>
+
+                                        <div class="author-info">
+                                            <h4 class="author">ChatGPT</h4>
+                                            <h5 class="company">AI Testimonials</h5>
+                                            <div class="icon">
+                                                <i class="fas fa-quote-right"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- End of Testimonial 3 -->
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End of Testimonials -->
