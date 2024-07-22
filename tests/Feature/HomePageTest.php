e6ad3f48e6b8da01ca62402093fe28d11d60f3cc
Andrew Atkinson <andrew.atkinson@visualsoft.co.uk>
2024-07-22 16:55:01 +0100

    removed comment

--- tests/Feature/HomePageTest.php
+++ tests/Feature/HomePageTest.php
@@ -4,5 +4,5 @@ it('loads the homepage', function () {
     $response = $this->get('/');
     
     $response->assertStatus(200);
-    $response->assertSee('Andrew Atkinson'); // Adjust 'Welcome' to a string that should appear on your homepage
+    $response->assertSee('Andrew Atkinson'); 
 });
