84d0044ded94b81cc15663f66b9b409302cbe429
Andrew Atkinson <andrew.atkinson@visualsoft.co.uk>
2024-07-25 00:21:40 +0100

    completed content

---
+++ resources/views/about-me.blade.php
@@ -0,0 +1,26 @@
+<div class="col-xs-12 col-sm-7">
+    <p>As a Technical Lead at Visualsoft for the past two years, I have had the privilege of guiding and mentoring multiple squads of talented developers. My role has encompassed leading 3-4 different teams, each with up to six developers, through a variety of innovative and impactful projects.</p>
+    <p>At Visualsoft, I have spearheaded projects in advanced technology areas such as facial recognition, enabling our clients to leverage cutting-edge capabilities. Additionally, I have contributed to developing multi-channel solutions, including work on platforms like TikTok and Debenhams, enhancing our clients' reach and engagement.</p>                            
+    <p>My approach to leadership is rooted in fostering a collaborative and innovative environment, ensuring that every team member has the support and resources they need to excel. I am passionate about staying at the forefront of technology trends and driving projects that not only meet but exceed client expectations.</p>
+</div>
+
+<div class="col-xs-12 col-sm-5">
+    <div class="info-list">
+        <ul>
+            <li>
+                <span class="title">Residence</span>
+                <span class="value">USA</span>
+            </li>
+
+            <li>
+                <span class="title">Current Role</span>
+                <span class="value">{{$experiences->first()->title}}</span>
+            </li>
+
+            <li>
+                <span class="title">e-mail</span>
+                <span class="value">atkinson.andy@gmail.com</span>
+            </li>
+        </ul>
+    </div>
+</div>
